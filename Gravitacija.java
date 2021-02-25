cdfe1e903c763920dce78ce861d35dc456a18c19
CookieSgot <gasper.jug999@gmail.com>
2021-02-25 17:46:00 +0100

    zdruzenje

--- Gravitacija.java
+++ Gravitacija.java
@@ -3,8 +3,6 @@ import java.util.Scanner;
 public class Gravitacija {
      public static void main(String args[]){
         Scanner sc = new Scanner(System.in);
-        // test!
-        Scanner sc = new Scanner(System.in);
         double visina = sc.nextInt();
         System.out.println(visina);
         System.out.println(gravitacija(visina));
