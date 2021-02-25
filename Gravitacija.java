12d9d6032998dfe127d144078010866df34bdb93
Rozles <rozlesterle@gmail.com>
2021-02-25 17:01:06 +0100

    popravek izpisa

--- Gravitacija.java
+++ Gravitacija.java
@@ -10,11 +10,4 @@ public class Gravitacija {
         System.out.println(gravitacija(visina));
         
     } 
-    public static double gravitacija(double visina) {
-        double C = 6.674 * Math.pow(-11,10); 
-        double M = 5.972 * Math.pow(24,10);
-        double R = 6.371 * Math.pow(6,10);
-
-        return (C * M) / Math.pow(2,(R + visina));
-    }
 }
