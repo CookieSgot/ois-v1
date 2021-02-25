5ac9a06fe9fe0dba8fea5e57d12272f4aceb72f0
CookieSgot <gasper.jug999@gmail.com>
2021-02-25 17:35:35 +0100

    izracun 2

--- Gravitacija.java
+++ Gravitacija.java
@@ -1,7 +1,18 @@
+import java.util.Scanner;
+
 public class Gravitacija {
     public static void main(String args[]){
         System.out.println("OIS je zakon!");
 
         // test!
-    } 
+        Scanner sc = new Scanner(System.in);
+        int visina = sc.nextInt();
+    }
+    public static double gravitacija(double visina) {
+        double C = 6.674 * Math.pow(-11,10); 
+        double M = 5.972 * Math.pow(24,10);
+        double R = 6.371 * Math.pow(6,10);
+
+        return (C * M) / Math.pow(2,(R + visina));
+    }
 }
